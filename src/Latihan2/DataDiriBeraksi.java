b1ae6846df9200ceb3a72057f86f778e216b9f7d
rafiandradsyf <murafida@gmail.com>
2025-06-12 11:57:04 +0800

    membuat objek

---
+++ src/Latihan2/DataDiriBeraksi.java
@@ -0,0 +1,16 @@
+
+package Latihan2;
+
+
+public class DataDiriBeraksi {
+    public static void main(String[] args) {
+        // Membuat objek dari class DataDiri
+        DataDiri saya = new DataDiri("Muhammad Rafiandra Darsyaf", "2310010217", "SI'23 A");
+
+        // Menjalankan metode tampilkan
+        saya.tampilkan();
+    }
+}
+
+    
+
